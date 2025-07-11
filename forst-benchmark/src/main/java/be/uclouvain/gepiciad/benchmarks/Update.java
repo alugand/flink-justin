f6de3bb216b9cb329a3080d108e6c40f7de90d2e
Arnaud Lugand <alugand@gmail.com>
2025-07-11 16:10:54 +0200

    fix: typo in folder name

---
+++ forst-benchmark/src/main/java/be/uclouvain/gepiciad/benchmarks/Update.java
@@ -0,0 +1,77 @@
+package be.uclouvain.gepiciad.benchmarks;
+
+import be.uclouvain.gepiciad.sources.Event;
+import be.uclouvain.gepiciad.sources.EventDeserializer;
+import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+import org.apache.flink.api.common.functions.OpenContext;
+import org.apache.flink.api.common.functions.RichFlatMapFunction;
+import org.apache.flink.api.common.state.v2.ValueState;
+import org.apache.flink.api.common.state.v2.ValueStateDescriptor;
+import org.apache.flink.api.common.typeutils.base.StringSerializer;
+import org.apache.flink.connector.kafka.source.KafkaSource;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.sink.v2.DiscardingSink;
+import org.apache.flink.util.Collector;
+import org.apache.flink.util.ParameterTool;
+
+public class Update {
+
+    public static void main(String[] args) throws Exception{
+        final ParameterTool pt = ParameterTool.fromArgs(args);
+
+        final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        String password = pt.get("kafka-pwd");
+        String jaasCfg = String.format(
+                "org.apache.kafka.common.security.scram.ScramLoginModule required username=\"user1\" password=\"%s\";",
+                password
+        );
+        KafkaSource<Event> source = KafkaSource.<Event>builder()
+                .setBootstrapServers("kafka-service.kafka.svc.cluster.local:9092")
+                .setTopics("event-demo")
+                .setGroupId("my-consumer-flink")
+                //.setStartingOffsets(OffsetsInitializer.latest())
+                .setDeserializer(new EventDeserializer())
+                .setProperty("security.protocol", "SASL_PLAINTEXT")
+                .setProperty("sasl.mechanism", "SCRAM-SHA-256")
+                .setProperty("sasl.jaas.config", jaasCfg)
+                .build();
+
+        env.fromSource(source, WatermarkStrategy.noWatermarks(),"KafkaSource")
+                .keyBy(Event::getKey)
+                .enableAsyncState()
+                .flatMap(new Mapper())
+                .sinkTo(new DiscardingSink<>());
+
+        env.execute("Simple Benchmark");
+    }
+
+    public static class Mapper extends RichFlatMapFunction<Event, String> {
+
+        private static final long serialVersionUID = 1L;
+
+        private transient ValueState<String> valueState;
+
+        @Override
+        public void open(OpenContext openContext) throws Exception {
+            super.open(openContext);
+            int index = getRuntimeContext().getTaskInfo().getIndexOfThisSubtask();
+            valueState =
+                    getRuntimeContext()
+                            .getState(
+                                    new ValueStateDescriptor<>(
+                                            "valueState" + index, StringSerializer.INSTANCE));
+        }
+
+        @Override
+        public void flatMap(Event event, Collector<String> out) throws Exception {
+            valueState.asyncValue().thenAccept(currentValue -> {
+                if (currentValue != null) {
+                    out.collect(currentValue);
+                }
+                valueState.asyncUpdate(event.getPayload());
+            });
+        }
+
+
+    }
+}
---
+++ forst-benchmark/src/main/java/be/uclouvain/gepiciad/sources/KafkaGenerator.java
@@ -0,0 +1,69 @@
+package be.uclouvain.gepiciad.sources;
+
+import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+import org.apache.flink.connector.kafka.sink.KafkaRecordSerializationSchema;
+import org.apache.flink.connector.kafka.sink.KafkaSink;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.util.ParameterTool;
+import org.apache.flink.formats.json.JsonSerializationSchema;
+import org.apache.flink.connector.datagen.source.DataGeneratorSource;
+import org.apache.flink.connector.datagen.source.GeneratorFunction;
+import java.util.Random;
+import org.apache.flink.api.common.typeinfo.TypeInformation;
+import org.apache.flink.api.connector.source.util.ratelimit.RateLimiterStrategy;
+import java.time.Instant;
+import org.apache.flink.util.StringUtils;
+
+public class KafkaGenerator {
+    public static void main(String[] args) throws Exception{
+        final ParameterTool pt = ParameterTool.fromArgs(args);
+        Random random = new Random();
+
+        final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        String password = pt.get("kafka-pwd");
+        String jaasCfg = String.format(
+                "org.apache.kafka.common.security.scram.ScramLoginModule required username=\"user1\" password=\"%s\";",
+                password
+        );
+
+
+        int srcRate = Integer.parseInt(pt.get("src-rate","10000"));
+        int payloadLength = Integer.parseInt(pt.get("payload-length","1000"));
+
+
+        GeneratorFunction<Long, Event> generatorFunction = i -> {
+            int key = random.nextInt(100000);
+            long eventTime = Instant.now().toEpochMilli();
+            i++;
+            String payload = StringUtils.getRandomString(
+                    random, payloadLength, payloadLength, 'A', 'z');
+            return new Event(key, eventTime, i, payload);
+        };
+
+        DataGeneratorSource<Event> dataGeneratorSource =
+                new DataGeneratorSource<>(
+                        generatorFunction,
+                        Long.MAX_VALUE,
+                        RateLimiterStrategy.perSecond(srcRate),
+                        TypeInformation.of(Event.class)
+                );
+
+        KafkaSink<Event> kafkaSink = KafkaSink.<Event>builder()
+                .setProperty("security.protocol", "SASL_PLAINTEXT")
+                .setProperty("sasl.mechanism", "SCRAM-SHA-256")
+                .setProperty("batch.size", "262144")
+                .setProperty("linger.ms", "50")
+                .setProperty("sasl.jaas.config", jaasCfg)
+                .setBootstrapServers("kafka-service.kafka.svc.cluster.local:9092")
+                .setRecordSerializer(KafkaRecordSerializationSchema.builder()
+                        .setValueSerializationSchema(new JsonSerializationSchema<Event>())
+                        .setTopic("event-demo")
+                        .build())
+                .build();
+
+        env.fromSource(dataGeneratorSource, WatermarkStrategy.noWatermarks(),"event-stream")
+                .sinkTo(kafkaSink);
+
+        env.execute("Kafka Event Generator Job");
+    }
+}
