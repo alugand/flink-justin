66550c8abfb85a433995d0a8cb8d2af68d16df3d
Arnaud Lugand <alugand@gmail.com>
2025-07-14 16:18:11 +0200

    fix: git repo for k8s cluster source

--- scripts/infra/g5k/main.tf
+++ scripts/infra/g5k/main.tf
@@ -1,8 +1,7 @@
 module "k8s_cluster" {
-    source = "github.com/guillaumerosinosky/terraform-grid5000-k8s-cluster"
+    source = "github.com/alugand/terraform-grid5000-k8s-cluster"
     nodes_count = 5 #8
     walltime = 1 #2
-    reservation = var.reservation
     nodes_selector="{cluster='gros'}"
     oar_job_name = "gepiciad_resource-estimator"
     kubernetes_version = "v1.22.4-rancher1-1"
